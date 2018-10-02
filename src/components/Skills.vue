70fe07d52a5df3b25ecef66d0b1e37aa57d264df
Rachana Acharya <rasnacharya@gmail.com>
2018-10-02 12:23:35 -0600

    Add thirdparty animation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,7 +5,7 @@
       <form @submit.prevent="addSkill">
       <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
       
-      <transition name="alert-in">
+      <transition name="alert-in" enter-active-class="animated wobble" leave-active-class="animated wobble">
       <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
       </transition>
 
@@ -49,6 +49,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
   .holder {
     background: #fff;
   }
