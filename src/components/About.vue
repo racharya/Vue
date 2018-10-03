d92d8afc572d5e02266471faefd9f65fa7e8791a
Rachana Acharya <rasnacharya@gmail.com>
2018-10-03 12:39:48 -0600

    Demonstrate the read router parameter

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
+    <h1>Hello, {{ input_name }} </h1>
     <h1>This is an about page</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return { 
+      input_name: this.$route.params.name
+    }
+  }  
+}
+</script>
